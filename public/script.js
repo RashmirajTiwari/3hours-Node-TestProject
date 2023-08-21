ebbe487dab52797f8817cefc218fa18d6748ece8
Rashmiraj Tiwari <rashmirajt@athmin.com>
2023-08-21 19:24:38 +0530

    Modified Code

--- public/script.js
+++ public/script.js
@@ -1,5 +1,8 @@
 var list = document.getElementById('items');
 var todoDone = document.getElementById('todoDone');
+var RTodo = document.getElementById('RTodo');
+var DTodo = document.getElementById('DTodo');
+
 function getAllTodo(){
     window.addEventListener("DOMContentLoaded", async () => {
         try {
@@ -10,8 +13,8 @@ function getAllTodo(){
                 <span class="span" style="display:none">${res.data[i].id}</span>
                 <span class="span" >${res.data[i].itemName}</span>
                 <span class="span" >${res.data[i].description}</span>
-                <button class="edit-btn">Edit</button>
-                <button class="delete-btn">Delete</button>
+                <button class="edit-btn">&check;</button>
+                <button class="delete-btn">X</button>
                 </li>`
             }
         } catch (error) {
@@ -28,21 +31,28 @@ var submit = document.getElementById("submitBtn");
 submit.addEventListener("click", async (e) => {
   
     try {
-        var TodoId = document.getElementById('TodoId');
         var itemName = document.getElementById('itemName');
         var description = document.getElementById('description');
         
         let todo= {
             itemName:itemName.value,
             description:description.value,
+           }
+          
+          
+          //Show heading Done and Remaing todo availabe or not;
+           function isEmpty(id) {
+            return document.getElementById(id).innerHTML== ""
+          }
+          if(isEmpty("items")==true){
+            RTodo.innerHTML=`<h3 style="text-align: center;" >Remaining Todo</h3>`
+          }else{
+            RTodo.innerHTML=``
+          }
+          
            
-
-        }
-    
-        const id=TodoId.value;
-    
-    const res=  await axios.post("http://localhost:3000/postTodo",todo);
-    showTodo(res);
+        const res=  await axios.post("http://localhost:3000/postTodo",todo);
+        showTodo(res);
    
    
      
@@ -65,8 +75,19 @@ list.addEventListener('click', async (e) => {
                 const li = e.target.parentElement;
                 const id = li.firstElementChild.innerText
                 list.removeChild(li);
+                
+
+          //Show heading Done and Remaing todo availabe or not;
+                function isEmpty(id) {
+                    return document.getElementById(id).innerHTML== ""
+                  }
+                  
+                 if(isEmpty("items")==true)
+                    RTodo.innerHTML=`` 
+               
+               
                 await axios.delete(`http://localhost:3000/deleteTodo/${id}`);
-              
+                
 
             }
         } 
@@ -84,10 +105,25 @@ list.addEventListener('click', async (e) => {
     try {
         if (e.target.classList.contains("edit-btn")) {
                 const li = e.target.parentElement;
-                console.log(li.children[1])
                 var itemName = li.children[1].innerText;
                 var description = li.children[2].innerText;
                 list.removeChild(li);
+
+
+          //Show heading Done and Remaing todo availabe or not;
+                function isEmpty(id) {
+                    return document.getElementById(id).innerHTML== ""
+                  }
+                  
+                console.log(isEmpty("items"))
+                if(isEmpty("items")==true)
+                RTodo.innerHTML=``
+                
+                if(isEmpty("DTodo")==false)
+                DTodo.innerHTML=`<h3 style="text-align: center;" >Done Todo</h3>`
+                
+                 
+                
     } 
         if(itemName!=undefined ||description!=undefined )
         todoDone.innerHTML += `<li><span class="span" >${itemName}</span><span class="span" >${description}</span></li>`
@@ -106,8 +142,8 @@ function showTodo(res){
         <span class="span" style="display:none">${res.data.id}</span>
         <span class="span" >${res.data.itemName}</span>
         <span class="span" >${res.data.description}</span>
-        <button class="edit-btn">Edit</button>
-        <button class="delete-btn">Delete</button>
+        <button class="edit-btn">&check;</button>
+        <button class="delete-btn">x</button>
         </li>`
     
 
